6a8f801e1f9909e26ad58f5710595f2842a57815
Johan Sannemo <jsannemo@jsannemo.se>
2018-02-28 08:37:21 +0100

    answers and input validator for pokemon

---
+++ pokemon/input_format_validators/validate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from sys import stdin
+import re, sys
+
+line = stdin.readline()
+r, c = map(int, line.split())
+goals = 0
+for _ in range(r):
+    line = stdin.readline()
+    for c in line:
+        if c == 'M':
+            goals += 1
+
+assert goals == 1
+
+sys.exit(42)
